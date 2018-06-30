a1883d58393dec9edcf3b11723dd6d2b87ab6daf
epalomares <edgaropalomaresm@gmail.com>
2018-06-30 17:53:38 -0400

    [fix] merge error

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -73,13 +73,8 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 //TODO Put here the name of your currency
-<<<<<<< HEAD
 const char     CRYPTONOTE_NAME[]                             = "konote";
 const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff01029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210174b337baf0c3947b26beed05a47d772ea4707247633659d69c4bd2e29df653e7";
-=======
-const char     CRYPTONOTE_NAME[]                             = "Konopy";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
->>>>>>> 612fc6d7de8ce501f7f7e4372799c8573d364cc3
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
